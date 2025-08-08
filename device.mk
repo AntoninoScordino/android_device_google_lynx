69efa316e69bb00d5eb8ff9c03025bbb3009622f
Antonino Scordino <git@ntonino.eu>
2025-08-08 22:55:13 +0200

    lynx: merge 'lineage' Makefiles into main ones

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ $(call inherit-product-if-exists, vendor/google_devices/lynx/proprietary/device-
 $(call inherit-product-if-exists, vendor/google_devices/lynx/proprietary/WallpapersLynx.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/google/lynx/lynx/overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/lynx/overlay-lineage
 
 include device/google/gs201/device-shipping-common.mk
 include device/google/gs-common/touch/gti/predump_gti.mk
@@ -369,3 +370,59 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 $(call inherit-product-if-exists, device/google/common/etm/device-userdebug-modules.mk)
 endif
 endif
+
+# ANGLE - Almost Native Graphics Layer Engine
+PRODUCT_PACKAGES += \
+    ANGLE
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlayLynx
+
+# Init
+PRODUCT_PACKAGES += \
+    init.recovery.lynx.touch.rc
+
+# IWLAN
+PRODUCT_PACKAGES += \
+    Iwlan
+
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
+
+# Build necessary packages for vendor
+
+# Codec2
+PRODUCT_PACKAGES += \
+    libacryl \
+    libacryl_hdr_plugin \
+    libexynosv4l2
+
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-V2-ndk.vendor:64
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libEGL_angle \
+    libGLESv1_CM_angle \
+    libGLESv2_angle
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwifi-hal-ctrl:64
